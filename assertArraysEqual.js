ecbca6ffde814eaa9fe6ff6909063b39cc00c9bb
max k <maxkuhn@gmail.com>
2022-06-28 23:19:06 -0700

    Made a change to assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -12,21 +12,30 @@ const eqArrays = function(array1, array2) {
 
 
 const assertArraysEqual = function(array1, array2) {
-  if (array1.length !== array2.length) {
+  if (!eqArrays(array1, array2)) {
     console.log(`🤡 Assertion Failed: ${array1} !== ${array2}`);
-    return;
+  } else {
+    console.log(`✨ Assertion Passed: ${array1} === ${array2}`);
   }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      console.log(`🤡 Assertion Failed: ${array1} !== ${array2}`);
-      return;
-    }
-  }
-  console.log(`✨ Assertion Passed: ${array1} === ${array2}`);
 };
 
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
-assertArraysEqual([1, 2, 3], [1, 2, "3"]);
+const without = function(source, itemsToRemove) {
+  let arrayToReturn = source.splice();
+  for (i = 0; i < source.length; i++) {
+    for (j = 0; j < itemsToRemove.length; j++) {
+      if (source[i] === itemsToRemove[j]) {
+        arrayToReturn.splice(i, 1);
+      }
+    }
+  }
+  return arrayToReturn;
+}
 
+let fruits = ["Banana", "Mango", "Strawberry", "Melon"];
+
+console.log("Fruits = " + fruits);
+without(fruits, ["Strawberry"]);
+console.log("Now fruits = " + fruits);
+
+assertArraysEqual(fruits, ["Banana", "Mango", "Strawberry", "Melon"]);
