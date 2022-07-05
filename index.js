b6a42cfa507775582f89fc37f783231fc2f7b679
max k <maxkuhn@gmail.com>
2022-07-05 00:19:22 -0700

    fixed bug in package.json

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // index.js
-const head   = require('./head');
-const tail   = require('./tail');
+const head = require('./head');
+const tail = require('./tail');
 const middle = require('./middle');
 const countLetters = require('./countLetters');
 const countOnly = require("./countOnly");
