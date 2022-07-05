d2040c16968ced85e02d467ed21665c3dbf4e66d
max k <maxkuhn@gmail.com>
2022-07-04 22:57:10 -0700

    Used Chai to test middle and tail

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,21 @@
+const assert = require('chai').assert;
 const tail = require("../tail");
-const assertEqual = require("../assertEqual");
+
+let parts = ["head", "shoulders", "knees", "toes"];
+
+describe("#tail", () => {
+  it("checks if the tail on an array of four words is three", () =>  {
+    assert.strictEqual(tail(parts).length, 3);
+  });
+  it("checks if a given tail is the right words", () =>  {
+    assert.deepEqual(tail(parts), ["shoulders", "knees", "toes"]);
+  });
+})
 
 
-console.log(tail(["head", "shoulders", "knees", "toes"]));
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+
+// console.log(tail(["head", "shoulders", "knees", "toes"]));
+// // Test Case: Check the original array
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3)
